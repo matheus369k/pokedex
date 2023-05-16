993c3e9dca5b21f30fb26b7429f8ac71f3b25870
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2023-05-16 12:18:26 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+
+const body = document.querySelector("body")
+
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
+
+
+
+botaoAlterarTema.addEventListener("click", () => {
+    const modoEscuroEstaAtivado = body.classList.contains("modo-escuro");
+
+    body.classList.toggle("modo-escuro")
+
+    if(modoEscuroEstaAtivado){
+        imagemBotaoTrocaDeTema.setAttribute("src","./imagens/sol2.gif");
+
+    }else{
+
+        imagemBotaoTrocaDeTema.setAttribute("src","./imagens/Moon_rotating_full_220px.gif");
+        body.classList.contains("modo-escuro");
+        }   
+})
