1f4813f44a9c413d5ded526b66782e1063e71dcc
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2023-11-10 19:08:32 -0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,7 +17,7 @@ function Home() {
     // ( handlebar ) responsavel por adicionar o estilo de selecionado ao cliclar e um cartão de uma regiao
     const handlebar=()=>{
         document.querySelector("#homeswitch").childNodes.forEach((gene, index)=> {
-            gene.addEventListener("mouseup", () => {
+            gene.addEventListener("click", () => {
                 
                 const navbar = document.getElementById("nav_bar")  
                 
@@ -32,7 +32,7 @@ function Home() {
     return (
         <Container customClass="Home">
             <h1>Selecione a Região</h1>
-            <ul id="homeswitch" onClick={()=>handlebar()}>
+            <ul id="homeswitch" onMouseEnter={()=>handlebar()} onTouchStart={()=>handlebar()}>
                 <Link to="/onegeneration">
                     <li>
                         <div><img src={logo} alt="Nome Pokemon" /></div>
