9aeff8436363bcc96fc38bf1c8f56df7eb2de509
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2024-03-29 19:11:28 -0300

    add loading pokedex

--- src/components/pokedex/imgPoke/PokeImg.js
+++ src/components/pokedex/imgPoke/PokeImg.js
@@ -1,8 +1,19 @@
+import { finnishLoadCard } from "../../../function/finnishLoad";
 import React from "react";
 import "./index.css";
 
 export default function PokeImg(getData) {
     return (
-        <img className="img_container" src={getData.dataImages} alt={`Pokedex ${getData.dataName}`} />
+        <img 
+            onLoad={()=>finnishLoadCard(
+                ".pokedex_container",
+                "pokedex_loading",
+                1,
+                ".pokedex_container"
+            )} 
+            className="img_container" 
+            src={getData.dataImages} 
+            alt={`Pokedex ${getData.dataName}`} 
+        />
     );
 }
