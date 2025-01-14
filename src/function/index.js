037d92d36f23622b0d77e86e7156b7bdc2c6ec53
matheus369k <gomes.craft10@gmail.com>
2025-01-14 19:50:30 -0300

    remove: finnish loading

--- src/function/index.js
+++ src/function/index.js
@@ -1,9 +1,4 @@
 export { backScrollTop } from './back-scroll-top';
-
 export { searchOfName } from './filter-of-name';
-
 export { searchOfNumber } from './filter-of-number';
-
-export { finnishLoadCard } from './finnish-load';
-
 export { getUrlState, setUrlState } from './url-state';
