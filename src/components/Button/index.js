8fbf277a413cf4ac0164def515f98ab8374e991a
matheus369k <gomes.craft10@gmail.com>
2025-01-14 19:45:07 -0300

    remove: Button component

--- src/components/Button/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './index.css';
-
-export function Button({...props}) {
-	return <button {...props} />;
-}
--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,5 @@
-export { Button } from './Button';
-
 export { Cards } from './Cards';
-
 export { Header } from './Header';
-
 export { Pokedex } from './Pokedex';
-
 export { Search } from './SearchBar';
-
 export { SearchOfRegions } from './SearchOfRegions';
