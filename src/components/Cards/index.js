b6b4513039057941e33722258172d3a4f71f78b7
matheus369k <gomes.craft10@gmail.com>
2025-01-14 19:48:06 -0300

    refactor: cards  files toggle from .css to .module.css

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,48 +1,51 @@
-import React, { useContext } from 'react';
-import './index.css';
-import './responsive.css';
-import { finnishLoadCard, searchOfNumber } from '../../function/index';
+import { useContext, useState } from 'react';
+import { searchOfNumber } from '../../function/index';
 import { PokemonDataContext } from '../../context/pokemon-datas';
+import styles from './index.module.css';
 
 export function Cards() {
 	const { state, handleAddSelected } = useContext(PokemonDataContext);
+	const [loading, setLoading] = useState(true);
+
+	function handleFinnishLoading() {
+		setLoading(false);
+	}
 
 	function handleOpenPokedex({ number }) {
 		const pokemonNumber = Number(number.split('#')[1]);
-		const getPokeDataForNumber = searchOfNumber(pokemonNumber, 1)
+		const getPokeDataForNumber = searchOfNumber(pokemonNumber, 1);
 
 		handleAddSelected(getPokeDataForNumber);
 	}
 
 	return (
-		<ul className="pokemons_container">
-			{state.data.map((element, index) => (
-				// biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
-				<li
-					onClick={() => handleOpenPokedex(element)}
-					id={element.name}
-					className={`pokemon-${index} card_loading`}
-					key={element.name}
-				>
-					<img
-						onLoad={() =>
-							finnishLoadCard(`.pokemon-${index}`, 'card_loading', index, '.pokemons_container>li')
-						}
-						src={`${element.images}`}
-						alt={`Pokemon: ${element.name}${element.number}`}
-					/>
-					<div>
-						<p>
-							<span>Pokedex: </span>
-							{element.number}
-						</p>
-						<p>
-							<span>Nome: </span>
-							{element.name}
-						</p>
-					</div>
-				</li>
-			))}
+		<ul className={styles.pokemon_card_list}>
+			{state.data.map((element) => {
+				return (
+					<li
+						onClick={() => handleOpenPokedex(element)}
+						{...(loading ? { className: styles.card_loading } : '')}
+						key={element.name}
+					>
+						<img
+							onLoad={handleFinnishLoading}
+							src={element.images}
+							loading='lazy'
+							alt={`Pokemon: ${element.name}-${element.number}`}
+						/>
+						<div>
+							<p>
+								<span>Pokedex: </span>
+								{element.number}
+							</p>
+							<p>
+								<span>Nome: </span>
+								{element.name}
+							</p>
+						</div>
+					</li>
+				);
+			})}
 		</ul>
 	);
 }
